75db948859db84108bfbc7e69fdca437288ab634
Liva <ausmoons@gmail.com>
2022-06-18 19:19:58 +0300

    Adding first steps

---
+++ Assets/Scripts/ObjectActions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectActions : MonoBehaviour
+{
+    public GameObject objectToHide;
+    public GameObject objectToShow;
+    public GameObject objectToShowNext;
+
+    public void Update()
+    {
+        if (objectToHide != null && objectToShow != null && Input.GetKeyDown(KeyCode.Space))
+        {
+            this.HideObject(objectToHide);
+            this.ShowObject(objectToShow);
+            this.ShowObject(objectToShowNext);
+        }
+    }
+
+    public void ShowObject(GameObject gameObject)
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void HideObject(GameObject gameObject)
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void ShowInShelf(GameObject gameObject)
+    {
+        gameObject.SetActive(false);
+    }
+}
